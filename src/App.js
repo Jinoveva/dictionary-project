4d8e342d7f2519554dd4b7fb45196e2e9a4f54eb
Jinoveva <jinovevalopes@gmail.com>
2025-05-01 13:03:52 +0200

    Search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       Jinoveva's Dictionary App
-      </header>
+      <div className="container">
+        <header className="App-header">Jinoveva's Dictionary App</header>
+        <main>
+          <Dictionary />
+        </main>
+        <small>
+          <footer className="text-center App-footer">Coded by Jinoveva</footer>
+        </small>
+      </div>
     </div>
   );
 }
